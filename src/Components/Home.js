9fdf7d862c27fe2742231997ef1de7ed40a5dca9
Anshuman Shukla <Anshumanshukla46@gmail.com>
2023-05-05 11:08:34 +0530

    hello

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
 
                 {/* taken same color with of logo */}
-                <p className='text-pink-600'>Hi, my name is</p>
+                <p className='text-pink-600'>Hello, my name is</p>
 
                 <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Anshuman Shukla</h1>
 
